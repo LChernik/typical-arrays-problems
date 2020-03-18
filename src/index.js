569144ba49cb9b1061b3569afe2c0e743b8140b2
LChernik <54461132+LChernik@users.noreply.github.com>
2020-03-17 22:31:32 -0400

    feat: add max, min, avg function solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!!array && array.length > 0) {
+        let min = Math.min(...array)
+        return min;
+    }
+    return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!!array && array.length > 0) {
+        let max = Math.max(...array)
+        return max;
+    }
+    return 0
 }
 
 exports.avg = function avg (array) {
+    if (!!array && array.length > 0) {
+        let sum = array.reduce(function (a,b) { return a+b},0);
+        let average = sum/array.length;
+        return average;
+    }
   return 0;
 }
